60d726f0eb9d542b5de49eeb3597a59e1eb6c029
Joycelynb65 <74356840+Joycelynb65@users.noreply.github.com>
2021-12-28 15:40:27 +0800

    Update Main.java

--- DSProject/src/Main.java
+++ DSProject/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			System.out.println(new GoogleQuery("Dove").query());
-//			GoogleQuery g =new GoogleQuery("Dove");
+			GoogleQuery g =new GoogleQuery("Dove");
 //			g.query();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
