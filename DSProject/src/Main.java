e87b491b776c94f4fab630165db22a50cab0ece7
Calla <brotherC0607@gmail.com>
2022-01-03 15:02:27 +0800

    1.From java to web project 2.Some jsp files(homepage)

--- DSProject/src/Main.java
+++ DSProject/src/Main.java
@@ -1,6 +1,16 @@
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
-public class Main {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Main{
 
 	public static void main(String[] args) {
 		try {
---
+++ DSProject/src/Project.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Project")
+public class Project extends HttpServlet{
+	
+	public Project() {
+		
+		super();
+		
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		if(request.getParameter("keyword")== null) {
+			String requestUri = request.getRequestURI();
+			request.setAttribute("requestUri", requestUri);
+			request.getRequestDispatcher("Search.jsp").forward(request, response);
+			return;
+		}
+		GoogleQuery google = new GoogleQuery(request.getParameter("keyword"));
+		HashMap<String, String> query = google.query();
+		
+		String[][] s = new String[query.size()][2];
+		request.setAttribute("query", s);
+		int num = 0;
+		for(Entry<String, String> entry : query.entrySet()) {
+		    String key = entry.getKey();
+		    String value = entry.getValue();
+		    s[num][0] = key;
+		    s[num][1] = value;
+		    num++;
+		}
+		request.getRequestDispatcher("googleitem.jsp")
+		 .forward(request, response); 
+		
+	}
+
+}
