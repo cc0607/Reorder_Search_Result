207b730169133a5b5d34e4e70d929fc8110c787b
WhitneyWen <74042761+humankill@users.noreply.github.com>
2022-01-10 23:48:51 +0800

    Update WebNode.java

--- DSProject/src/WebNode.java
+++ DSProject/src/WebNode.java
@@ -1,9 +1,9 @@
-
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class WebNode {
-	
+	public WebNode parent;
+	public ArrayList<WebNode> children;
 	public WebPage webPage;
 	public double nodeScore;//main element This node's score += all its children¡¦s nodeScore
 	
@@ -22,6 +22,29 @@ public class WebNode {
 		nodeScore = webPage.score;
 		
 		}
+	
+	public void addChild(WebNode child){
+		//add the WebNode to its children list
+		this.children.add(child);
+		child.parent = this;
+	}
+	
+	public boolean isTheLastChild(){
+		if(this.parent == null) return true;
+		ArrayList<WebNode> siblings = this.parent.children;
+		
+		return this.equals(siblings.get(siblings.size() - 1));
+	}
+	
+	public int getDepth(){
+		int retVal = 1;
+		WebNode currNode = this;
+		while(currNode.parent!=null){
+			retVal ++;
+			currNode = currNode.parent;
+		}
+		return retVal;
+	}
 		
 				
 			
